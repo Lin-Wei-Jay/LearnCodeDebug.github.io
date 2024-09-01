33784f59667f0e0fa73e34b641c3ac8cf083cf0c
NOBTG <139665972+NOBTG@users.noreply.github.com>
2024-09-01 15:02:39 +0900

    modify

--- script.js
+++
@@ -1,7 +0,0 @@
-window.onkeyup = keyup;
-function keyup(e) {
-  var inputTextValue = e.target.value;
-  if (e.keyCode == 13) {
-    window.location = "https://duckduckgo.com/?q=" + inputTextValue;
-  }
-}
